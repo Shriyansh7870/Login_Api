2cd72078e2f4089ad6b74a2e2cbfc825430c5da5
Shriyansh Kumar <71579575+Shriyansh7870@users.noreply.github.com>
2023-10-24 16:17:33 +0530

    a

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ app.use(express.json());
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api", userRouter);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4000");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 app.listen(4000, () => {
   console.log(`Server is running fine`);
 });
